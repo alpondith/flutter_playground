85f9b77184c553e0ccf8f9603cf46f797ab04d6a
alpondith <sujonalimran@gmail.com>
2021-12-04 09:13:06 +0600

    play sound branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
-import 'bangladesh/flag.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() {
-  runApp(Flag());
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  AudioPlayer audioPlayer = AudioPlayer();
+  App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton.icon(
+            onPressed: () {
+              audioPlayer.play(
+                "assets/beep.mp3",
+                isLocal: true,
+              );
+            },
+            icon: Icon(Icons.play_arrow, size: 18),
+            label: Text("Play"),
+          ),
+        ),
+      ),
+    );
+  }
 }
